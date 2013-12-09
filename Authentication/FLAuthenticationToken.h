f095d1741c74a43bc5a6714c2964ae8268752566
Mike Fullerton <mike@Mikes-MacPro.local>
2013-12-08 18:24:33 -0800

    move files to sensible location

---
+++ Authentication/FLAuthenticationToken.h
@@ -0,0 +1,34 @@
+//
+//  FLAuthenticationToken.h
+//  FishLampCore
+//
+//  Created by Mike Fullerton on 11/15/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FishLampMinimum.h"
+
+@protocol FLAuthenticationSession <NSObject, NSCopying>
+- (BOOL) hasExpired;
+- (void) touchAuthenticationTimestamp;
+@end
+
+@interface FLAuthenticationToken : NSObject<FLAuthenticationSession> {
+@private
+    id _token;
+    NSTimeInterval _authTokenLastUpdateTime;
+    NSTimeInterval _lastAuthenticationTimestamp;
+    NSTimeInterval _timeoutInterval;
+}
+
+@property (readonly, strong, nonatomic) NSString* token;
+@property (readonly, assign, nonatomic) NSTimeInterval lastAuthenticationTimestamp;
+@property (readonly, assign, nonatomic) NSTimeInterval authTokenLastUpdateTime;
+@property (readwrite, assign, nonatomic) NSTimeInterval timeoutInterval;
+
++ (id) authenticationToken:(NSString*) authenticationToken;
+
+- (void) resetAuthenticationTimestamp;
+- (void) touchAuthenticationTimestamp;
+
+@end
---
+++ Authentication/FLCredentialsProvider.m
@@ -0,0 +1,10 @@
+//
+//  FLCredentialsProvider.m
+//  FishLampCore
+//
+//  Created by Mike Fullerton on 11/12/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FLCredentialsProvider.h"
+
