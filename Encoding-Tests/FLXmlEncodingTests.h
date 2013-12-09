43033005842d090f4dc1eeb52f1c802021cc12d2
Mike Fullerton <mike@Mikes-MacPro.local>
2013-12-08 18:24:33 -0800

    move files to sensible location

---
+++ Encoding-Tests/FLXmlEncodingTests.h
@@ -0,0 +1,13 @@
+//
+//  FLXmlEncodingTests.h
+//  FishLampCore
+//
+//  Created by Mike Fullerton on 10/27/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FLTestable.h"
+
+@interface FLXmlEncodingTests : FLTestable
+
+@end
