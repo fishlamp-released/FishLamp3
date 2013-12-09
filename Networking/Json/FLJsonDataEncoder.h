933c9c8362cab6c3b6dce22892ee455bfb2c6880
Mike Fullerton <mike@Mikes-MacPro.local>
2013-12-08 18:24:33 -0800

    move files to sensible location

---
+++ Networking/Json/FLJsonDataEncoder.h
@@ -0,0 +1,24 @@
+//
+//  FLJsonDataEncoder.h
+//  FishLamp
+//
+//  Created by Mike Fullerton on 5/11/11.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton.
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FishLampMinimum.h"
+
+#import "FLStringToObjectConversionManager.h"
+
+@interface FLJsonDataEncoder : FLStringToObjectConversionManager {
+@private
+}
+
+FLSingletonProperty(FLJsonDataEncoder);
+
+@end
+
+@interface NSString (FLJsonDataEncoder)
+- (NSString*) jsonEncode;
+@end
