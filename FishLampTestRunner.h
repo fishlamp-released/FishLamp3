3de16fb970e9c6e55d2ba9539b7a63f9a6480272
Mike Fullerton <mike@greentongue.com>
2013-12-31 15:50:41 -0800

    update after removing OSX prerelease

---
+++ FishLampTestRunner.h
@@ -0,0 +1,16 @@
+//
+//  FLRunAllTestsOperation.h
+//  FishLampCore
+//
+//  Created by Mike Fullerton on 10/19/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FishLampCore.h"
+#import "FishLampAsync.h"
+
+#import "FLRunAllTestsOperation.h"
+#import "FLTestLoggingManager.h"
+#import "FLTestOrganizer.h"
