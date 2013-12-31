4032eed860f5cbc843fd27f24b769c81cc523d28
Mike Fullerton <mike@greentongue.com>
2013-12-31 15:50:41 -0800

    update after removing OSX prerelease

---
+++ FLUserDefaultsCredentialStorage.h
@@ -0,0 +1,30 @@
+//
+//  FLUserDefaultsCredentialStorage.h
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 5/3/13.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLCredentialsStorage.h"
+#import "FLAuthenticationCredentials.h"
+
+@interface FLUserDefaultsCredentialStorage : NSObject<FLCredentialsStorage>
+FLSingletonProperty(FLUserDefaultsCredentialStorage);
+@end
+
+//@interface FLAuthenticationCredentials (NSUserDefaults)
+//+ (id) authCredentialsFromUserDefaults;
+//
+//- (void) writeToUserDefaults;
+//- (void) writePasswordToKeychain;
+//
+//- (void) readFromUserDefaults;
+//- (void) readPasswordFromKeychain;
+//- (void) removePasswordFromKeychain;
+//
+//
+//
+//@end
+
