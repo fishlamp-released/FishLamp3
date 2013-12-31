416a8fd5f8efa28959824fe940f98939d48c80df
Mike Fullerton <mike@greentongue.com>
2013-12-31 15:50:41 -0800

    update after removing OSX prerelease

---
+++ FLPromisedResult.h
@@ -0,0 +1,25 @@
+//
+//  FLPromisedResult.h
+//  FishLampCocoa
+//
+//  Created by Mike Fullerton on 6/23/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FishLampCore.h"
+
+/**
+ *  Abstract type for a unfufilled promise. This will either be an error or a valid value.
+ */
+#define FLPromisedResult id
+
+@interface NSObject (FLPromisedResult)
+- (BOOL) isError;
++ (id) fromPromisedResult:(FLPromisedResult) promisedResult;
+@end
+
+@protocol FLPromisedResultContract <NSObject>
+- (BOOL) isError;
+@end
+
+#define FLPromisedResultType(__TYPE__) FLPromisedResult
---
+++ Misc/FLCompletion.m
@@ -0,0 +1,13 @@
+//
+//  FLCallback.m
+//  FishLampCore
+//
+//  Created by Mike Fullerton on 11/10/13.
+//  Copyright (c) 2013 Mike Fullerton. All rights reserved.
+//
+
+#import "FLCompletion.h"
+
+@implementation FLCompletion
+
+@end
