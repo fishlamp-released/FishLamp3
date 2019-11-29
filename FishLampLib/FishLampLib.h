685d681eb971843a64d2fbbc7dfa36920e43d909
Mike Fullerton <mike@mikefullerton.com>
2019-11-29 12:11:26 -0800

    update

---
+++ FishLampLib/FishLampLib.h
@@ -0,0 +1,13 @@
+//
+//  FishLampLib.h
+//  FishLampLib
+//
+//  Created by Mike Fullerton on 10/16/19.
+//  Copyright © 2019 Fiesty Bytes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FishLampLib : NSObject
+
+@end
---
+++ FishLampLib/FishLampLib.m
@@ -0,0 +1,13 @@
+//
+//  FishLampLib.m
+//  FishLampLib
+//
+//  Created by Mike Fullerton on 10/16/19.
+//  Copyright © 2019 Fiesty Bytes. All rights reserved.
+//
+
+#import "FishLampLib.h"
+
+@implementation FishLampLib
+
+@end
